2902e83da091367aaedc638c0af2981c5a7c6da5
ishaan766 <ishaan766@gmail.com>
2025-03-03 13:52:35 +0530

    # New Relic Instrumentation

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import requests
 import mysql.connector
 import redis
+import newrelic.agent
 from fastapi import FastAPI
 
 app = FastAPI()
+
+newrelic.agent.initialize()
 
 cnx = mysql.connector.connect(
     user='root', password='root', host='mysql', database='test')
